5d021af3dc3ccbc2eced806fe5363f5ce2c851fe
Eric Franz <efranz@osc.edu>
2019-06-21 11:50:02 -0400

    example with results page

--- app.rb
+++ app.rb
@@ -10,4 +10,33 @@ class App < Sinatra::Base
   get "/" do
     erb :index
   end
+  
+  get "/jobs" do
+    output = `/opt/torque/bin/qselect`
+    @jobs = output.split
+
+    erb :jobs
+  end
+
+  get "/job" do
+      @jobid = params['jobid']
+      @jobstats = `/opt/torque/bin/qstat -f #{@jobid}`
+
+      @results = []
+      if params['output'] && File.exist?(params['output'])
+        @results = Dir.glob("#{params['output']}/*png")
+      end
+
+      erb :job
+  end
+
+  get "/new" do
+    erb :new
+  end
+
+  post "/job" do
+    jobid = "123456"
+    output = Pathname.new(params['inputfile']).dirname.join('output').to_s
+    redirect to("/job?jobid=#{jobid}&output=#{output}")
+  end
 end
