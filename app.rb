ab0266f0564acc0c0ddac5b51d0477759fd94052
Eric Franz <efranz@osc.edu>
2019-06-24 13:20:57 -0400

    add flash and job submission

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require "sinatra/base"
 
 class App < Sinatra::Base
   set :erb, :escape_html => true
+  enable :sessions
+
+  # flash message support
+  before do
+    @flashnow = session[:flash] || {}
+  end
+
+  after do
+    session[:flash] = @flash || {}
+  end
 
   def title
     "My App"
@@ -35,8 +45,20 @@ class App < Sinatra::Base
   end
 
   post "/job" do
-    jobid = "123456"
-    output = Pathname.new(params['inputfile']).dirname.join('output').to_s
-    redirect to("/job?jobid=#{jobid}&output=#{output}")
+    #TODO: error handling!
+    #
+    # submit job
+    inputfile = Pathname.new(params['inputfile'])
+    inputdir = inputfile.dirname
+    outputdir = inputdir.join('output')
+
+    # ahh - debugging this is problematic... that is where flash message is useful... you can have error msg or output msg then
+    # could do "submitted job with command: ....."
+    cmd = "/opt/torque/bin/qsub -v BLEND_FILE_PATH=#{inputfile.to_s} -d #{inputdir.to_s} /users/PZS0562/efranz/ondemand/dev/jobs/jobs/blender_render_job.sh 2>&1".strip
+    jobid = `#{cmd}`
+
+    @flash = {info: "Job submitted using command: #{cmd} with output: #{jobid}"}
+
+    redirect to("/job?jobid=#{jobid}&output=#{outputdir.to_s}")
   end
 end
