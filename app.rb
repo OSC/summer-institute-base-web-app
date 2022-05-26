ad6dc79ceda06c714614efc450e4a97231601b5a
Jeff Ohrstrom <johrstrom@osc.edu>
2022-05-26 15:23:43 -0400

    add logger and give a new title

--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 require 'sinatra/base'
+require 'logger'
 
 # App is the main application where all your logic & routing will go
 class App < Sinatra::Base
   set :erb, escape_html: true
 
+  logger = Logger.new('log/app.log')
+
   def title
-    'My App'
+    'Summer Instititue Starter App'
   end
 
   get '/' do
+    logger.info('requsting the index')
     erb :index
   end
 end
