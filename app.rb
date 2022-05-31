a1e4ba9de436eecc09c275bdee476e3e3d72c557
Jeff Ohrstrom <johrstrom@osc.edu>
2022-05-31 13:21:19 -0400

    submit a job that runs

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
 
   get '/' do
     logger.info('requsting the index')
-    @flash = { info: 'Welcome to Summer Institute!' }
+    @flash = { info: 'Welcome to Summer Institute!' } if @flash.nil?
     erb :index
   end
 
@@ -40,6 +40,45 @@ class App < Sinatra::Base
   post '/render/frames' do
     logger.info("Trying to render frames with: #{params.inspect}")
 
-    erb :render_frames_new
+    if params['blend_file'].nil?
+      blend_file = "#{input_files_dir}/#{params[:uploaded_blend_file]}"
+    else
+      blend_file = "#{input_files_dir}/#{params['blend_file'][:filename]}"
+      copy_upload(input: params['blend_file'][:tempfile], output: blend_file)
+    end
+
+    output_dir = "#{project_root}/jobs/#{Time.now.to_i}".tap { |p| Dir.mkdir(p) unless Pathname.new(p).exist? }
+    basename = File.basename(blend_file, '.*')
+    walltime = format('%02d:00:00', params[:num_hours])
+
+    args = ['-J', "blender-#{basename}", '--parsable']
+    args.concat ['--export', "BLEND_FILE_PATH=#{blend_file},OUTPUT_DIR=#{output_dir},FRAMES_RANGE=#{params[:frames_range]}"]
+    args.concat ['-n', params[:num_cpus], '-t', walltime, '-M', 'pitzer']
+    args.concat ['--output', "#{output_dir}/#{basename}-%j.out"]
+    output = `/bin/sbatch #{args.join(' ')}  #{project_root}/render_frames.sh 2>&1`
+
+    job_id = output.strip.split(';').first
+    `echo #{job_id} > #{output_dir}/job_id`
+
+    @flash = { info: "submitted job #{job_id}" }
+    erb :index
+  end
+
+  def copy_upload(input: nil, output: nil)
+    input_sha = Pathname.new(input).file? ? Digest::SHA256.file(input) : nil
+    output_sha = Pathname.new(output).file? ? Digest::SHA256.file(output) : nil
+    return if input_sha.to_s == output_sha.to_s
+
+    File.open(output, 'wb') do |f|
+      f.write(input.read)
+    end
+  end
+
+  def project_root
+    __dir__
+  end
+
+  def input_files_dir
+    Pathname.new("#{project_root}/jobs/input_files")
   end
 end
