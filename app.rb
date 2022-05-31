16a352fb72ea444954c4ec9e7c99458c4300583a
Jeff Ohrstrom <johrstrom@osc.edu>
2022-05-31 12:48:49 -0400

    add flashes

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require 'logger'
 # App is the main application where all your logic & routing will go
 class App < Sinatra::Base
   set :erb, escape_html: true
+  enable :sessions
 
   attr_reader :logger
 
@@ -20,6 +21,7 @@ class App < Sinatra::Base
 
   get '/' do
     logger.info('requsting the index')
+    @flash = { info: 'Welcome to Summer Institute!' }
     erb :index
   end
 end
