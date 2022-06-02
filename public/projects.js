2d1f6b9190c1abe3a433a49e5ffd03107277e5b3
Jeff Ohrstrom <johrstrom@osc.edu>
2022-06-02 16:43:44 -0400

    add javascript to update carousel automatically

--- public/projects.js
+++ public/projects.js
@@ -5,6 +5,8 @@ window.onload = () => {
   const blend_file_input = document.getElementById('blend_file');
   blend_file_input.addEventListener('change', (event) => showFileName(event));
 
+  udpateCarousel();
+
   function showFileName(event) {
 
     // Get label where the file name will be displayed.
@@ -16,5 +18,59 @@ window.onload = () => {
 
     // Change the label to display the file name.
     infoArea.textContent = 'File chosen: ' + fileName;
+  }
+
+  function udpateCarousel() {
+    const opts = {
+      follow: true,
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+
+    const configEle = document.getElementById('job_config');
+    const directory = configEle.getAttribute('data-directory');
+
+    fetch(`/pun/sys/dashboard/files/fs/${directory}`, opts)
+      .then(response => response.json())
+      .then(data => data['files'])
+      .then(files => files.map(file => file['name']))
+      .then(files => files.filter(file => file.endsWith('png')))
+      .then(files => {
+        for(const file of files) {
+          const id = `image_${file.replaceAll('.', '_')}`;
+          const ele = document.getElementById(id);
+
+          if(ele == null) {
+            console.log(`adding ${file} to carousel`);
+
+            const carousel = document.getElementById('blend_image_carousel_inner');
+            const carouselList = document.getElementById('blend_image_carousel_indicators');
+            const listSize = carouselList.children.length;
+
+            const newImg = document.createElement('div')
+            newImg.id = id;
+            newImg.classList.add('carousel-item');
+            newImg.innerHTML = `<img class="d-block w-100" src="/pun/sys/dashboard/files/fs/${directory}/${file}" >`;
+
+            const newIndicator = document.createElement('li');
+            newIndicator.setAttribute('data-target', '#blend_image_carousel');
+            newIndicator.setAttribute('data-slide-to', listSize+1);
+
+            const listSpan = document.getElementById('list_size');
+            listSpan.innerHTML = `${listSize+1}`;
+
+            if(listSize == 0) {
+              newImg.classList.add('active');
+              newIndicator.classList.add('active');
+            }
+
+            carousel.append(newImg);
+            carouselList.append(newIndicator);
+          }
+        }
+      });
+
+    setTimeout(udpateCarousel, 5000);
   }
 };
