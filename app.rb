5c3cc97bae151efcd31e9646482871842147cb1b
Mayur Chermala <mayurc77@pitzer-rw02.ten.osc.edu>
2022-06-09 16:44:55 -0400

    Created project directory and updated the index

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   set :erb, escape_html: true
   enable :sessions
 
-  attr_reader :logger
+  attr_reader :logger, :groups
 
   def initialize
     super
@@ -16,12 +16,57 @@ class App < Sinatra::Base
   end
 
   def title
-    'Summer Instititue Starter App'
+    'Summer Institute - Blender'
+  end
+
+  def input_files_dir
+    "#{project_dirs}/input_files"
+  end
+
+  def projects_root
+    "#{__dir__}/projects"
+  end
+
+  get '/projects/:dir' do
+    if params[:dir] == 'new' || params[:dir] == 'input_files'
+      erb :new_project
+    else
+      @dir = Pathname("#{projects_root}/#{params[:dir]}")
+      @flash = session.delete(:flash)
+
+      unless @dir.directory? || @dir.readable?
+        session[:flash] = { danger: "#{@dir} does not exist" }
+        redirect(url('/'))
+      end
+    erb :show_project
+    end
+  end
+
+  post '/projects/new' do
+    logger.info("Trying to render frames with: #{params.inspect}")
+
+    dir = params[:name].downcase.gsub(' ', '_')
+    "#{projects_root}/#{dir}".tap{ |d| FileUtils.mkdir_p(d) }
+
+    session[:flash] = { info: "made new project '#{params[:name]}'" }
+    redirect(url("/projects/#{dir}"))
+  end
+
+  post 'render/frames' do
+    session[:flash] = { info: "rendering frames with '#{params}'" }
+    redirect(url('/'))
+  end
+
+  def project_dirs
+    Dir.children(projects_root).reject{ |dir| dir == 'input_files' }.sort_by(&:to_s)
   end
 
   get '/' do
     logger.info('requsting the index')
-    @flash = { info: 'Welcome to Summer Institute!' }
+    @flash = session.delete(:flash) || {info: 'Welcome to Summer Institute!'}
+
+    @project_dirs = project_dirs
+
     erb :index
   end
 end
