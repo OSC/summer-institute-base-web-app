a25c0d5d08b76bde117a5b7a88dfe97a743c0574
Mayur Chermala <mayurc77@pitzer-rw02.ten.osc.edu>
2022-06-10 11:45:07 -0400

    Able to submit a job and render frames

--- public/projects.js
+++ public/projects.js
@@ -1,19 +1,15 @@
 window.onload = () => {
 
-    // Setup the helper to show the filename that you uploaded
+    // Shows file name
     const blend_file_input = document.getElementById('blend_file');
     blend_file_input.addEventListener('change', (event) => showFileName(event));
   
     function showFileName(event) {
+      const info_area = document.getElementById('blend_file_name');
   
-      // Get label where the file name will be displayed.
-      const infoArea = document.getElementById('blend_file_name');
-  
-      // Get the file input where a new file was selected.
       const input = event.srcElement;
-      const fileName = input.files[0].name;
+      const file_name = input.files[0].name;
   
-      // Change the label to display the file name.
-      infoArea.textContent = 'File chosen: ' + fileName;
+      info_area.textContent = 'File chosen: ' + file_name;
     }
   };
